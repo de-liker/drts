dec384498fc0fd7a6ce507460f58ba4938f9c7ce
Qs-F <qsf@de-liker.com>
2023-11-25 16:09:46 +0900

    Update to support flat config

---
+++ eslint.config.js
@@ -0,0 +1,108 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import ts from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import importPlugin from 'eslint-plugin-import'
+import reactPlugin from 'eslint-plugin-react'
+
+const compat = new FlatCompat()
+
+export default [
+  ...compat.extends('eslint-config-google').map((config) => ({
+    ...config,
+    ignores: ['dist/**/*'],
+  })),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    ignores: ['dist/**/*'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+      import: importPlugin,
+      react: reactPlugin,
+    },
+    rules: {
+      ...ts.configs['recommended'].rules,
+      ...ts.configs['eslint-recommended'].rules,
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      'require-jsdoc': 'off',
+      'max-len': [
+        'error', {
+          code: 160,
+          ignoreComments: true,
+          ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+      'array-element-newline': [
+        'error',
+        'consistent',
+      ],
+      'object-curly-spacing': [
+        'error',
+        'always',
+      ],
+      'quote-props': [
+        'error',
+        'as-needed',
+      ],
+      'react/prop-types': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/display-name': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: false,
+        },
+      ],
+      'import/no-unresolved': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/namespace': [
+        'off',
+        {
+          allowComputed: true,
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          pathGroupsExcludedImportTypes: [
+            'react',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: '/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+]
